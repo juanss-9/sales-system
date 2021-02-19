87be2bbe96d1eaa14cbb245d1bff0b6a594d0974
juanss-9 <79232677+juanss-9@users.noreply.github.com>
2021-02-19 15:54:13 -0500

    Add files via upload

---
+++ Sale.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales
+{
+    public class Sale
+    {
+        public Product Product;
+        public SalesMan SalesMan;
+        public string Comments;
+    }
+}
